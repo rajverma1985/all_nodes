4bfe4169499b6b8efcd95c9969d4a020220c98f1
Raj Verma <Rjnshvrm23@gmail.com>
2016-12-05 00:54:13 +0530

    added the berks file

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'all_nodes'
 maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'rjnshvrm23@example.com'
 license 'all_rights'
 description 'Installs/Configures all_nodes'
 long_description 'Installs/Configures all_nodes'
-version '0.1.0'
+version '0.1.1'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
@@ -13,3 +13,5 @@ version '0.1.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/all_nodes' if respond_to?(:source_url)
+
+depends 'chef-client'
